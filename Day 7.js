744e76a7337caf323893db8489f2a4b139aae42b
Baptiste Marsigli <baptistemarsigli@gmail.com>
2022-12-07 15:48:28 +0100

    tabulation

--- Day 7.js
+++ Day 7.js
@@ -1,28 +1,28 @@
 {
-  const input = document.querySelector('pre').innerText.split('$ ')
-	
-  class File {
-    constructor(name, size) {
-      this.name = name
-      this.size = size
-    }
-  }
-  class Folder extends File{
-    constructor(parent, name, children) {
+	const input = document.querySelector('pre').innerText.split('$ ')
+
+	class File {
+		constructor(name, size) {
+			this.name = name
+			this.size = size
+		}
+	}
+	class Folder extends File{
+		constructor(parent, name, children) {
 			super(name, 0)
-      this.parent = parent
-      this.children = children
+			this.parent = parent
+			this.children = children
 			this.invalidate_size()
-    }
+		}
 		add(...children) {
 			this.children.push(...children)
 			this.invalidate_size()
 		}
-    invalidate_size() {
-      this.size = this.children.reduce((acc, cur) => acc + cur.size, 0)
+		invalidate_size() {
+			this.size = this.children.reduce((acc, cur) => acc + cur.size, 0)
 			this.parent?.invalidate_size()
-    }
-  }
+		}
+	}
 
 	const folders_repertoire = []
 	let current_folder = null
@@ -34,7 +34,7 @@
 
 		if (current_folder)
 			current_folder.children.push(folder)
-		
+
 		return folder
 	}
 
@@ -42,13 +42,13 @@
 		if (name === '..') {
 			current_folder = current_folder.parent ?? current_folder
 		} else {
-      let folder = current_folder?.children?.find(c => c instanceof Folder && c.name === name)
-      if (!folder) {
-        folder = make_dir(name)
+			let folder = current_folder?.children?.find(c => c instanceof Folder && c.name === name)
+			if (!folder) {
+				folder = make_dir(name)
 				root = folder
 			}
-      current_folder = folder
-    }
+			current_folder = folder
+		}
 	}
 
 	function ls(list) {
@@ -64,7 +64,7 @@
 		})
 		current_folder.add(...files)
 	}
-	
+
 	input.forEach(buffer => {
 		if (buffer.startsWith('cd')) {
 			cd(buffer.slice(3, buffer.length-1))
@@ -82,7 +82,7 @@
 
 	folders_repertoire.sort((a, b) => a.size - b.size)
 
-  console.table({
+	console.table({
 		'Sum of the smallest directories': folders_repertoire.filter(f => f.size <= 100000).reduce((acc, f) => acc + f.size, 0),
 		'Smallest directory that could be deleted to free enough space': {folder: folders_repertoire.find(f => f.size >= additional_free_space_needed)}
 	})
