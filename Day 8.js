78129d211bc7eec2160af1508d734907f38e69b5
Baptiste Marsigli <baptistemarsigli@gmail.com>
2022-12-08 17:36:32 +0100

    Create Day 8.js

--- Day 8.js
+++ Day 8.js
@@ -1,86 +1,86 @@
 {
 	const input = document.querySelector('pre').innerText.split('\n').filter(l => l !== '')
-	const trees = input.map(l => [...l].map(t => ({ height: ~~t, visible: false })))
+	const forest = input.map(l => [...l].map(t => ({ height: ~~t, visible: false, score: 0 })))
 	
 	let r = -1
 	let c = -1
-	let last_height = -1
 
-	const directions = () => [
+	const directions_factory = () => [
 		{
 			init: () => r = c = -1,
 			next_row: () => (
 				c = -1,
-				++r < trees.length
+				++r < forest.length
 			),
-			next_column: () => ++c < trees[0].length
+			next_column: () => ++c < forest[0].length,
+			visibility: 0
 		},
 		{
 			init: () => r = c = -1,
 			next_row: () => (
 				r = -1,
-				++c < trees[0].length
+				++c < forest[0].length
 			),
-			next_column: () => ++r < trees.length
+			next_column: () => ++r < forest.length,
+			visibility: 0
 		},
 		{
-			init: () => { r = trees.length; c = trees[0].length },
+			init: () => { r = forest.length; c = forest[0].length },
 			next_row: () => (
-				c = trees[0].length,
+				c = forest[0].length,
 				--r > -1
 			),
-			next_column: () => --c > -1
+			next_column: () => --c > -1,
+			visibility: 0
 		},
 		{
-			init: () => { r = trees.length; c = trees[0].length },
+			init: () => { r = forest.length; c = forest[0].length },
 			next_row: () => (
-				r = trees.length,
+				r = forest.length,
 				--c > -1
 			),
-			next_column: () => --r > -1
+			next_column: () => --r > -1,
+			visibility: 0
 		}
 	]
 	
-	directions().forEach(d => {
+	directions_factory().forEach(d => {
 		d.init()
 
 		while (d.next_row()) {
-			last_height = -1
+			let last_height = -1
 
 			while (d.next_column()) {
-				if (trees[r][c].height > last_height) {
-					trees[r][c].visible = true
-					last_height = trees[r][c].height
+				if (forest[r][c].height > last_height) {
+					forest[r][c].visible = true
+					last_height = forest[r][c].height
 				}
 			}
 		}
 	})
 
-	trees.forEach((line, i) => line.forEach((t, j) => {
-		const t_directions = directions()
-		
-		t_directions.forEach(d => {
+	forest.forEach((line, i) => line.forEach((t, j) => {
+		t.score = directions_factory().reduce((acc, d) => {
+			let last_height = -1
 			r = i
 			c = j
 			
-			d.visibility = 0
-
 			while (d.next_column()) {
 				++d.visibility
 
-				if (trees[r][c].height < t.height)
-					last_height = trees[r][c].height
+				if (forest[r][c].height < t.height)
+					last_height = forest[r][c].height
 				else break
 			}
-		})
 
-		t.score = t_directions.reduce((acc, d) => acc * d.visibility, 1)
+			return acc * d.visibility
+		}, 1)
 	}))
 
-	const forest = trees.flat()
+	const trees = forest.flat()
 
   	console.table({
-		'Sum of the smallest directories': forest.filter(t => t.visible).length,
-		'Highest visibility score': Math.max(...forest.map(t => t.score))
+		'Sum of the smallest directories': trees.filter(t => t.visible).length,
+		'Highest visibility score': Math.max(...trees.map(t => t.score))
 	})
 }
